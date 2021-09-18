bffb081b5237ae41da0d4ca4bdaecd88baf75d43
Seniv Pavlo <senivpavlo016@gmail.com>
2021-09-18 18:16:34 +0300

    Add laba 2 (git force)

---
+++ Laba2/main.py
@@ -0,0 +1,43 @@
+# List
+# Task 1
+# a = []
+# for i in range(int(input())):
+#  a.append(int(input()))
+# print(a)
+# length_a = len(list(a))
+# print('Length: ' +  str(length_a))
+# last_user = a[-1]
+# a.remove(last_user)
+# print(a)
+
+# Task 2
+# from random import randrange
+# a1=[]
+# n = int(input())
+# for i in range(n):
+#   a1.append(randrange(1, 10))
+# print(a1)
+# sum_list = a + a1
+# print(sum_list)
+
+# Task 3
+# word = str(input())
+# list_word = list(word)
+# print(list_word)
+
+# Error exclusion
+# Task 1
+# try:
+# блок команд
+# except Error:
+# # Если не сработал try и объявлена
+# ошибка Error
+# finally:
+# # Выполняется в любом случае
+
+# Main Task
+print("My menu")
+print("1: create list")
+print("2: add element")
+print("3: preview")
+print("4: exit")
