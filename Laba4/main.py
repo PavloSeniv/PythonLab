16ab191dff0cb9185f636fa11b73eb8d6e04768d
Seniv Pavlo <senivpavlo016@gmail.com>
2021-09-30 15:40:24 +0300

    Add laba 4

---
+++ Laba4/main.py
@@ -0,0 +1,27 @@
+# Task 1
+a=[[0] * 5 for i in range(4)]
+print("Task 1:", a)
+
+print()
+print("#####################")
+print()
+##################################################
+
+# Task 2
+print("Task 2 Matrix 1")
+a=[[0, 1, 2, 3, 4] * 1 for i in range(5)]
+for i in range(5):
+  print(' '.join(map(str, a[i])))
+
+print()
+print("#####################")
+print()
+
+print("Task 2 Matrix 2")
+a=[[0, 1, 2, 3, 4] * 1 for i in range(5)]
+for i in range(5):
+  print(' '.join(map(str, a[i])))
+
+print()
+print("#####################")
+print()
