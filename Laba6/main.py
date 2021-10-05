5fba5934e46b6ab422e2912dba0a4491d1febba2
Seniv Pavlo <senivpavlo016@gmail.com>
2021-10-05 19:33:31 +0300

    Add task gerator

--- Laba6/main.py
+++ Laba6/main.py
@@ -22,3 +22,61 @@ print("Радіус кола вписаного в квадрат:", radius_circ
 #########################################
 
 # Task generator
+print()
+
+kn_2_file = "KN-2.csv"
+lines = [line for line in open(kn_2_file)]
+for i in lines:
+    # print(i)
+    continue
+
+list_line = (s.rstrip().split(",") for s in lines)
+next(list_line)
+
+student_1 = next(list_line)
+student_2 = next(list_line)
+student_3 = next(list_line)
+student_4 = next(list_line)
+student_5 = next(list_line)
+student_6 = next(list_line)
+student_7 = next(list_line)
+
+student_list = [student_1,student_2,student_3,student_4,student_5,student_6,student_7]
+# print(student_list)
+
+while True:
+    for student_item in student_list:
+        student = student_item
+    surname_student = student[0]
+    lab1 = student[1]
+    if lab1 == '+':
+        lab1 = 2
+    else:
+        lab1 = 0
+
+    lab2 = student[2]
+    if lab2 == '+':
+        lab2 = 2
+    else:
+        lab2 = 0
+
+    lab3 = student[3]
+    lab3 = list(lab3)
+    lab3 = int(lab3[0]) + int(lab3[2])
+    # print(lab3)
+
+    test = int(student[4])
+    test_20 = test / 5
+    test = test_20
+    # print(test)
+
+    kr1 = int(student[5])
+
+    sum_all_points = str(lab1 + lab2 + lab3 + test + kr1)
+    # print(sum_all_points)
+
+    student_result = open('student_result.txt', 'wt')
+    student_result.write(surname_student + "|")
+    student_result.write(sum_all_points)
+    break
+print("Работа закінчена")
