f807f1ede903ec806c6037f38415b75e6efd79eb
Seniv Pavlo <senivpavlo016@gmail.com>
2021-10-05 15:52:25 +0300

    Done task module

---
+++ Laba6/main.py
@@ -0,0 +1,24 @@
+# Task Module
+from radius_and_pow import radius_pow
+
+res2 = radius_pow.my_pow2(2.0)
+print("Піднесення до квадрату:", res2)
+
+res3 = radius_pow.my_pow3(2.0)
+print("Піднесення до кубу:", res3)
+
+diagonal_square= radius_pow.diagonal(2)
+print("Діагональ квадрату:", diagonal_square)
+
+perimeter_square = radius_pow.perimeter(2)
+print("Периметр квадрату:", perimeter_square)
+
+square_area = radius_pow.square(2)
+print("Площа квадрату:", square_area)
+
+radius_circle_inscribed_square = radius_pow.radius_circle(2)
+print("Радіус кола вписаного в квадрат:", radius_circle_inscribed_square)
+
+#########################################
+
+# Task generator
---
+++ Laba6/radius_and_pow/radius_pow.py
@@ -0,0 +1,29 @@
+# Піднесення до квадрату
+def my_pow2(x):
+    return x ** 2
+
+# Піднесення до кубу
+def my_pow3(x):
+    return x ** 3
+
+# Квадрат
+
+# Обчислення діагоналі
+def diagonal(a):
+    return a * 2 ** (0.5)
+
+# Периметр квадрата
+def perimeter(a):
+    return 4*a
+
+# Площа квадрата
+def square(a):
+    return a ** 2
+
+# Радіус кола вписаного в квадрат
+def radius_circle(a):
+    return a / 2
+
+if __name__ ==  "__main__":
+    print('   ')
+
